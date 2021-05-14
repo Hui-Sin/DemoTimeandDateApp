35b36e4a7aac9637b8b67bb1c102eb77f9af45ca
Hui-Sin <20018354@myrp.edu.sg>
2021-05-14 11:48:59 +0800

    Phase 1 Done

--- app/src/main/java/sg/edu/rp/c346/id20018354/demotimeanddateapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20018354/demotimeanddateapp/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id20018354.demotimeanddateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
@@ -11,19 +12,50 @@ import android.widget.TimePicker;
 public class MainActivity extends AppCompatActivity {
     DatePicker dp;
     TimePicker tp;
-    Button btndisplayDate;
-    Button btndisplayTime;
+    Button btnDisplayDate;
+    Button btnDisplayTime;
     TextView tvDisplay;
+    Button btnReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dp=findViewById(R.id.datePicker);
-        tp=findViewById(R.id.timePicker);
-        btndisplayDate=findViewById(R.id.buttonDisplayDate);
-        btndisplayTime=findViewById(R.id.buttonDisplayTime);
-        tvDisplay=findViewById(R.id.textViewDisplay);
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime = findViewById(R.id.buttonDisplayTime);
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnReset =findViewById(R.id.buttonReset);
+
+        btnDisplayTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String time="Time is"+tp.getCurrentHour()+":"+tp.getCurrentMinute();
+                tvDisplay.setText(time);
+            }
+        });
+        btnDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int day=dp.getDayOfMonth();
+                int month=dp.getMonth()+1;
+                int year=dp.getYear();
+                tvDisplay.setText("Date is "+day+"/"+month+"/"+year);
+
+            }
+        });
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tp.setCurrentHour(0);
+                tp.setCurrentMinute(0);
+                dp.updateDate(2021,0,1);
+
+            }
+        });
+
+
     }
 }
