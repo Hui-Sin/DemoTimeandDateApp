298f9c48e64a48ec2e65ea5fea41636689085deb
Hui-Sin <20018354@myrp.edu.sg>
2021-05-14 10:42:13 +0800

    Question 1 and 2 Done

--- app/src/main/java/sg/edu/rp/c346/id20018354/demotimeanddateapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20018354/demotimeanddateapp/MainActivity.java
@@ -3,12 +3,27 @@ package sg.edu.rp.c346.id20018354.demotimeanddateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+    DatePicker dp;
+    TimePicker tp;
+    Button btndisplayDate;
+    Button btndisplayTime;
+    TextView tvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dp=findViewById(R.id.datePicker);
+        tp=findViewById(R.id.timePicker);
+        btndisplayDate=findViewById(R.id.buttonDisplayDate);
+        btndisplayTime=findViewById(R.id.buttonDisplayTime);
+        tvDisplay=findViewById(R.id.textViewDisplay);
     }
 }
